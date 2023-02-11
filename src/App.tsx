00a5f53c0951eceec2b58f056a5f64d51e02fdee
AlexisRqs <alexisrqs@outlook.fr>
2023-02-11 18:21:34 +0100

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -12,11 +12,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/AlexisRqs/csr-movie-app"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          GitHub Repository
         </a>
       </header>
     </div>
