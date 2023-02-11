682e7fac8b45ad1496a606d35d0a0af5273e3d28
AlexisRqs <alexisrqs@outlook.fr>
2023-02-11 18:34:23 +0100

    Changed Learn React for repository URL & h1, h2

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>CSR Movie App</h1>
+        <h2>by Alexis.R</h2>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
